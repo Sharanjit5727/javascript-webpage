55e9e516409378e3609f24e6a5811b534bf124cc
Sharanjit Kaur <sharanjit955@gmail.com>
2020-08-19 16:44:39 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+function cake() {
+    document.getElementById("dn").style.display = "none";
+    document.getElementById("cupcake").style.display = "none";
+    document.getElementById("sweet").style.display = "none";
+    document.getElementById("cake").style.display = "block";
+  }
+  function cc() {
+    document.getElementById("cake").style.display = "none";
+    document.getElementById("dn").style.display = "none";
+    document.getElementById("sweet").style.display = "none";
+    document.getElementById("cupcake").style.display = "block";
+  }
+  function sweet() {
+    document.getElementById("cake").style.display = "none";
+    document.getElementById("cupcake").style.display = "none";
+    document.getElementById("dn").style.display = "none";
+    document.getElementById("sweet").style.display = "block";
+  }
+  function dn() {
+    document.getElementById("cake").style.display = "none";
+    document.getElementById("cupcake").style.display = "none";
+    document.getElementById("sweet").style.display = "none";
+    document.getElementById("dn").style.display = "block";
+  }
